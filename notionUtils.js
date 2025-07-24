6459f7d0e779f9856270828b48b62be2642674e4
Brayden May <127856333+brayd3nmay@users.noreply.github.com>
2025-07-24 16:31:13 -0400

    Update fetchActionItems time, error, return type
    Changed the time to be for Eastern Time Zone. Made error handling more robust. Function no longer returns an empty array on an error. Function now returns an array of pages rather than just page id's.

--- notionUtils.js
+++ notionUtils.js
@@ -4,73 +4,55 @@ const { Client } = require('@notionhq/client');
 const notion = new Client({ auth: process.env.NOTION_KEY });
 
 // Get the page ids for the action items that have been assigned and are past due
-async function getIds() {
-    let pageIds = [];
+async function fetchActionItems() {
+    let pastDueActionItems = [];
     let hasMore = true;
     let startCursor = undefined;
+    const today = new Date().toLocaleDateString('en-CA', { timeZone: 'America/New_York', year: 'numeric', month: '2-digit', day: '2-digit' });
 
     try {
+        const databaseId = process.env.NOTION_DATABASE_ID;
+        const filter = {
+            'and': [
+                {
+                    'property': 'Due Date',
+                    'date': { 'before': today }
+                },
+                {
+                    'or': [
+                        { 'property': 'Status', 'status': { 'equals': 'Assigned' } },
+                        { 'property': 'Status', 'status': { 'equals': 'Past Due' } },
+                        { 'property': 'Status', 'status': { 'equals': 'Delegated' } },
+                        { 'property': 'Status', 'status': { 'equals': 'In Progress' } }
+                    ]
+                }
+            ]
+        };
+
         while(hasMore) {
-            const databaseId = process.env.NOTION_DATABASE_ID;
             const response = await notion.databases.query({
                 database_id: databaseId,
                 start_cursor: startCursor,
-                filter: {
-                    'and': [
-                        {
-                            'property': 'Due Date',
-                            'date': {
-                                'before': new Date().toISOString().split('T')[0]
-                            }
-                        },
-                        {
-                            'or': [
-                                {
-                                    'property': 'Status',
-                                    'status': {
-                                        'equals': 'Assigned'
-                                    }
-                                },
-                                {
-                                    'property': 'Status',
-                                    'status': {
-                                        'equals': 'Past Due'
-                                    }
-                                },
-                                {
-                                    'property': 'Status',
-                                    'status': {
-                                        'equals': 'Delegated'
-                                    }
-                                },
-                                {
-                                    'property': 'Status',
-                                    'status': {
-                                        'equals': 'In Progress'
-                                    }
-                                }
-                            ]
-                        }
-                    ]
-                },
+                filter: filter,
             });
-
-            console.dir(response, { depth: null });
             
-            for(const page of response.results) {
-                const id = page.id;
+            pastDueActionItems.push(...response.results);
+            console.log(`Fetched batch of ${response.results.length} past-due action items. Total so far: ${pastDueActionItems.length}`);
 
-                pageIds.push(id);
-            }
-            
             hasMore = response.has_more;
             if (hasMore) {
                 startCursor = response.next_cursor;
+                console.log('There are more action items to fetch.');
+                console.log(`Next start cursor is ${startCursor}`);
+            }
+            else {
+                console.log('There are no more action items to fetch.');
             }
         }
 
-        return pageIds;
+        return pastDueActionItems;
     } catch (error) {
-        return [];
+        console.error(`\nERROR with status ${error.status} fetching past due action items:\n\n${error.message}`, error);
+        throw new Error(`Failed to fetch past due action item(s): ${error.message}`);
     }
 }
