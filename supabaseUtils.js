94ec6934bebed2d37c445b32777dd9bdb7c421a8
Brayden May <127856333+brayd3nmay@users.noreply.github.com>
2025-09-01 13:24:34 -0400

    Add variables for addEmail

--- supabaseUtils.js
+++ supabaseUtils.js
@@ -4,21 +4,21 @@ import { createClient } from '@supabase/supabase-js'
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
 
 
-async function addEmail () {
+async function addEmail (actionItemPageId, initiativeId, recipientId, recipientEmail, originalDueDate, currentDueDate, category, providerName, providerMessageId) {
     try {
         const data = await supabase
             .from('sent_emails')
             .insert([
                 {
-                    page_id: '',
-                    initiative_id: '',
-                    recipient_id: '',
-                    recipient_email: '',
-                    original_due_date: '',
-                    current_due_date: '',
-                    category: '',
-                    provider_name: '',
-                    provider_message_id: '',
+                    action_item_page_id: actionItemPageId,
+                    initiative_id: initiativeId,
+                    recipient_id: recipientId,
+                    recipient_email: recipientEmail,
+                    original_due_date: originalDueDate,
+                    current_due_date: currentDueDate,
+                    category: category,
+                    provider_name: providerName,
+                    provider_message_id: providerMessageId,
                 }
             ])
             .select()
